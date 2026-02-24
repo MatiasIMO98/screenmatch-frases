6b0c7f41376139c9ba8df3e32f8ec4538c7b3259
MatiasIMO98 <mimanriquezortiz@gmail.com>
2026-02-23 21:37:20 -0300

    Initial commit

---
+++ src/main/java/com/aluracursos/screenmatch_frases/model/Frase.java
@@ -0,0 +1,42 @@
+package com.aluracursos.screenmatch_frases.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "frases")
+public class Frase {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String frase;
+    private String personaje;
+    private String poster;
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getFrase() {
+        return frase;
+    }
+    public String getPersonaje() {
+        return personaje;
+    }
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+    public void setPersonaje(String personaje) {
+        this.personaje = personaje;
+    }
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+}
---
+++ src/main/java/com/aluracursos/screenmatch_frases/model/FraseDTO.java
@@ -0,0 +1,7 @@
+package com.aluracursos.screenmatch_frases.model;
+
+public record FraseDTO(String titulo,
+        String frase,
+        String personaje,
+        String poster) {
+}
